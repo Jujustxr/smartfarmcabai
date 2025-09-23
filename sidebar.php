3a321491e32f0f1d2feefeee08008e91b45b1571
Juju <magixcjane0@gmail.com>
2025-09-23 16:13:01 +0700

    Initial commit

---
+++ sidebar.php
@@ -0,0 +1,43 @@
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+                    <div class="sb-sidenav-menu-heading">Core</div>
+                    <a class="nav-link" href="index.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+                    <div class="sb-sidenav-menu-heading">Factors</div>
+                    <a class="nav-link" href="humidity.php">
+                        <div class="sb-nav-link-icon"><i class="fa-solid fa-droplet fa-bounce"></i></div>
+                        Humidity
+                    </a>
+                    <a class="nav-link" href="heatindex.php">
+                        <div class="sb-nav-link-icon"><i class="fa-solid fa-fire fa-fade"></i></div>
+                        Heat-Index
+                    </a>
+                    <a class="nav-link" href="soilMoisture.php">
+                        <div class="sb-nav-link-icon"><i class="fa-solid fa-plant-wilt fa-fade"></i></div>
+                        Soil-Moisture
+                    </a>
+                    <a class="nav-link" href="temp.php">
+                        <div class="sb-nav-link-icon"><i class="fa-solid fa-temperature-three-quarters fa-beat-fade"></i></div>
+                        Temperature
+                    </a>
+                    <a class="nav-link" href="waterLv.php">
+                        <div class="sb-nav-link-icon"><i class="fa-solid fa-water fa-beat"></i></div>
+                        Water-Level
+                    </a>
+                    <a class="nav-link" href="gasLv.php">
+                        <div class="sb-nav-link-icon"><i class="fa-solid fa-fire-flame-curved fa-beat"></i></div>
+                        Gas-Level
+                    </a>
+                </div>
+            </div>
+            <div class="sb-sidenav-footer">
+                <div class="small">Logged in as:</div>
+                Start Bootstrap
+            </div>
+        </nav>
+    </div>
